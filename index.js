99d825df90fef578e3440755916874a232b80afb
Brian Rebimbas <brebimbas@intelliquip.com>
2018-12-04 14:53:06 -0500

    update

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 module.exports = Phrase;
 
-String.prototype.reverse = function reverse() {
-    return Array.from(this).reverse().join("");
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
 }
 
-
-// Defines a Phrase object
+// Defines a Phrase object.
 function Phrase(content) {
-    this.content = content;
-    
-// Returns contenet processed for palindrome testing.
-this.processedContent = function processedContent() {
+  this.content = content;
+
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
     return this.content.toLowerCase();
-}
+  }
 
-//Returns true for a palindrom, false otherwise
-    this.palindrom = function palindrom (){
-        return this.processedContent() === reverse(this.processedContent());
-    }
+  // Returns true if the phrase is a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
+  }
 }
-
 
---
+++ test/test.js
@@ -0,0 +1,12 @@
+let assert = require("assert");
+let Phrase = require("../index.js");
+
+describe("Phrase", function() {
+  // Phrase#palindrome
+  describe("#palindrome", function() {
+
+    it("should return false for a non-polindrome", function() {
+        assert(true);
+    });
+  });
+});
